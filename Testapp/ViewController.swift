21873ca55274545c69fdad4ce654cc909a01bef6
Michael Kjemtrup <mckjemtrup@gmail.com>
2017-02-22 23:57:41 +0100

    starter

--- Testapp/ViewController.swift
+++ Testapp/ViewController.swift
@@ -9,17 +9,38 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var TextBox1: UITextField!
+    
+    
+    @IBOutlet weak var TextBox2: UITextField!
+    
+    @IBAction func Buttonpushed(_ sender: Any) {
+        
+        var addition = false
+        
+        if addition {
+            Labeltext3.text = "Svaret er...\(Double(TextBox1.text!)! + Double(TextBox2.text!)!)"
+        } else {
+            Labeltext3.text = "Svaret er...\(Double(TextBox1.text!)! - Double(TextBox2.text!)!)"
+        }
+        
+    }
+    
+    
+    @IBOutlet weak var Labeltext3: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
 }
 
